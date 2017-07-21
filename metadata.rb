313121af6cb975a9bcbfd4a3090b24f751b9d60e
James Wickett <wickett@gmail.com>
2017-07-21 15:33:14 -0500

    Added redhat for supermarket capability and apache license to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'signalsciences'
 maintainer 'Signal Sciences Corp.'
 maintainer_email 'support@signalsciences.com'
-license 'all_rights'
+license 'Apache-2.0'
 description 'Installs/Configures signalsciences'
 long_description 'Installs/Configures signalsciences'
 version '0.3.0'
@@ -9,7 +9,7 @@ chef_version '>= 12'
 issues_url 'https://github.com/signalsciences/signalsciences-cookbook/issues'
 source_url 'https://github.com/signalsciences/signalsciences-cookbook'
 
-%w( rhel centos ubuntu debian ).each do |os|
+%w( redhat centos ubuntu debian ).each do |os|
   supports os
 end
 
